9947325fb79af97df99177663a227f121ba60b8b
hainanricespecial <samuelsilversterling@gmail.com>
2025-02-26 15:59:15 +1000

    Added comments to routes.py to cause less confusion and minor fix to JSON (brackets on the wrong function)

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -2,31 +2,16 @@ from flask import Blueprint, jsonify
 
 api = Blueprint('api', __name__, url_prefix='/api/v1') 
 
+# Check status of API routes
 @api.route('/health')
-
 def health():
     return jsonify({"status": "ok"})
 
 # Get ALL of the to-dos
 @api.route('/todos', methods=['GET'])
-
 def get_todos():
-    return jsonify({
-        "id": 1,
-        "title": "Watch CSSE6400 Lecture",
-        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-        "completed": True,
-        "deadline_at": "2023-02-27T00:00:00",
-        "created_at": "2023-02-20T00:00:00",
-        "updated_at": "2023-02-20T00:00:00"
-    })
-
-# Get a specific to-do
-@api.route('/todos/<int:id>', methods=['GET'])
-
-def get_todo(id):
     return jsonify([{
-        "id": id,
+        "id": 1,
         "title": "Watch CSSE6400 Lecture",
         "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
         "completed": True,
@@ -35,9 +20,21 @@ def get_todo(id):
         "updated_at": "2023-02-20T00:00:00"
     }])
 
+# Get a specific to-do
+@api.route('/todos/<int:id>', methods=['GET'])
+def get_todo(id):
+    return jsonify({
+        "id": id,
+        "title": "Watch CSSE6400 Lecture",
+        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
+        "completed": True,
+        "deadline_at": "2023-02-27T00:00:00",
+        "created_at": "2023-02-20T00:00:00",
+        "updated_at": "2023-02-20T00:00:00"
+    })
+
 # Create a to-do
 @api.route('/todos', methods=['POST'])
-
 def create_todo():
     return jsonify({
         "id": 1,
@@ -51,7 +48,6 @@ def create_todo():
 
 # Update a to-do
 @api.route('/todos/<int:id>', methods=['PUT'])
-
 def update_todo(id):
     return jsonify({
         "id": id,
@@ -65,7 +61,6 @@ def update_todo(id):
 
 # Delete a to-do
 @api.route('/todos/<int:id>', methods=['DELETE'])
-
 def delete_todo(id):
     return jsonify({
         "id": id,
