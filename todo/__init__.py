0c6a198c86503133078eeec3dc10adcc68066671
hainanricespecial <samuelsilversterling@gmail.com>
2025-02-26 15:48:08 +1000

    Temporary fix for autograding system via turning off JSON auto-sort in Flask.

--- todo/__init__.py
+++ todo/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    app.json.sort_keys = False # temporary fix for autosorted JSON
     
     from .views.routes import api
     app.register_blueprint(api)
